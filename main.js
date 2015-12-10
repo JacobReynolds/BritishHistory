31b931b25950b5b4afc4d79d4608416f52a68f17
pny <reyno511@umn.edu>
2015-12-10 13:41:35 -0600

    mobile responsive

--- main.js
+++ main.js
@@ -7,41 +7,98 @@ https://color.adobe.com/create/color-wheel/?base=2&rule=Custom&selected=0&name=8
 
 //http://www.history.ac.uk/reviews/review/1480
 //15,000 pounds was anglo wealth in 1000
-$(document).ready(function () {
-    var tweets = ['lanfranc', 'william', 'money'];
-    var sections = $('.info');
-    var tweetDuration = 5;
-    var defaultHeight = $('#test').outerHeight();
 
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+//We set the height below to itself first to override 'auto'
+//Auto doesn't use CSS animations, so we override it for a bit
+
+var tweets = ['lanfranc', 'william', 'money'];
+var sections = $('.info');
+var tweetDuration = 5;
+//How many seconds the transition lasts for in the css
+var animationDuration = 1;
+//How much padding .info has
+var elementPadding = 5;
+var mobileNavOpen = false;
+
+function closeSection(section) {
+    $(section).addClass('inProgress');
+    $(section).css('height', $(section).height() + 'px');
+    var id = $(section).attr('id');
+    var content = $('#' + id + ' .content');
+    var h1 = $('#' + id + ' h1');
+    $(section).css('height', h1.height() + 'px');
+    setTimeout(function () {
+        content.css('display', 'none');
+        $(section).removeClass('active');
+        $(section).removeClass('inProgress');
+        $(section).css('height', 'auto');
+    }, animationDuration * 1000)
+}
+
+//We set the height below to itself first to override 'auto'
+//Auto doesn't use CSS animations, so we override it for a bit
+function openSection(section) {
+    $(section).addClass('inProgress');
+    $(section).css('height', $(section).height() + 'px');
+    var id = $(section).attr('id');
+    var content = $('#' + id + ' .content');
+    content.css('display', 'block');
+    $(section).css('height', $(section)[0].scrollHeight - (elementPadding * 2) + 'px');
+    setTimeout(function () {
+        $(section).removeClass('inProgress');
+        $(section).addClass('active');
+        $(section).css('height', 'auto');
+    }, animationDuration * 1000)
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setMobileNav() {
+    if (mobileNavOpen) {
+        closeSection($('#mobileNav'));
+    } else {
+        openSection($('#mobileNav'));
     }
+    mobileNavOpen = !mobileNavOpen;
+}
+
+function tweetSwitcher(index) {
+    if (index < tweets.length - 1)
+        index++;
+    else
+        index = 0;
+    $('#tweetWatcher').css('background', 'url(images/' + tweets[index] + 'tweet.jpg' + ')');
+    setTimeout(function () {
+        tweetSwitcher(index);
+    }, tweetDuration * 1000);
+}
+
+$(document).ready(function () {
 
 
+    //On click open the section and close all the others
     $('.info').click(function (event) {
-        if ($(this).outerHeight() === defaultHeight) {
-            $(this).css('height', $(this)[0].scrollHeight + 'px');
+        if ($(this).hasClass('inProgress'))
+            return;
+
+        if ($(this).hasClass('active')) {
+            closeSection(this);
         } else {
-            $(this).css('height', defaultHeight);
+            openSection(this);
         }
         for (var i = 0; i < sections.length; i++) {
             var element = sections[i];
             if ($(this).attr('id') != $(element).attr('id')) {
-                $(element).css('height', defaultHeight);
+                closeSection(element);
             }
         }
     })
 
-    function tweetSwitcher(index) {
-        if (index < tweets.length - 1)
-            index++;
-        else
-            index = 0;
-        $('#tweetWatcher').css('background', 'url(images/' + tweets[index] + 'tweet.jpg' + ')');
-        setTimeout(function () {
-            tweetSwitcher(index);
-        }, tweetDuration * 1000);
-    }
+    setTimeout(function () {
+        openSection($('.info')[0]);
+    })
 
     tweetSwitcher(getRandomInt(-1, tweets.length - 1));
 

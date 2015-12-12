0251053689d5e50d1f4cc9052c1a7a5971899054
pny <reyno511@umn.edu>
2015-12-12 17:04:24 -0600

    style changes added france tweet

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
-/*
-color scheme
-
-https://color.adobe.com/create/color-wheel/?base=2&rule=Custom&selected=0&name=800px-TinternAbbeyandCourtyard&mode=rgb&rgbvalues=0.32941176470588235,0.592156862745098,0.7647058823529411,0.5137254901960784,0.7098039215686275,0.8431372549019608,0.5372549019607843,0.5411764705882353,0.17647058823529413,0.45098039215686275,0.42745098039215684,0.1450980392156863,0.6549019607843137,0.5490196078431373,0.45098039215686275&swatchOrder=0,1,2,3,4
-
-*/
-
-//http://www.history.ac.uk/reviews/review/1480
-//15,000 pounds was anglo wealth in 1000
-
-//We set the height below to itself first to override 'auto'
+//We set the height in closeSection and openSection to itself first to override 'auto'
 //Auto doesn't use CSS animations, so we override it for a bit
 
-var tweets = ['lanfranc', 'william', 'money'];
+var tweets = ['lanfranc', 'william', 'money', 'france'];
 var sections = $('.info');
 var tweetDuration = 5;
 //How many seconds the transition lasts for in the css
